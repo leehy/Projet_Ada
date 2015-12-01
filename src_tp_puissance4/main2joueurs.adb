c0255f96da40dbdede91daf907fc7d5beb80f888
Hyun-Jai LEE <Hyun-Jai.lee@phelma.grenoble-inp.fr>
2015-12-01 19:15:26 +0100

    ajout de main2joueur

--- src_tp_puissance4/main2joueurs.adb
+++ src_tp_puissance4/main2joueurs.adb
@@ -4,7 +4,7 @@ with Puissance4;
 with Participant;
 with Partie;
 with Liste_Generique;
---with Moteur_Jeu;
+with Moteur_Jeu;
 
 use Ada.Text_IO;
 use Ada.Integer_Text_IO;
@@ -14,6 +14,18 @@ procedure Main2Joueurs is
    
    package MyPuissance4 is new Puissance4('X', 'O', '-',4,7,7);
    
+   package MyMoteur_jeu is new Moteur_Jeu(MyPuissance4.Etat,
+					  MyPuissance4.Coup, 
+					  MyPuissance4.Etat_Suivant, 
+					  MyPuissance4.Est_Gagnant,
+					  MyPuissance4.Est_Nul,
+					  MyPuissance4.Affiche_Coup,
+					  MyPuissance4.Liste_Coups,
+					  MyPuissance4.Coups_Possibles,
+					  MyPuissance4.Eval,
+					  2,
+					  Joueur1);
+use MyMoteur_jeu;
    -- definition d'une partie entre L'ordinateur en Joueur 1 et un humain en Joueur 2
    package MyPartie is new Partie(MyPuissance4.Etat,
 				  MyPuissance4.Coup, 
@@ -24,7 +36,8 @@ procedure Main2Joueurs is
 				  MyPuissance4.Est_Nul,
 				  MyPuissance4.Affiche_Jeu,
 				  MyPuissance4.Affiche_Coup,
-				  MyPuissance4.Coup_Joueur1,
+				  --MyPuissance4.Coup_Joueur1,
+				  MyMoteur_Jeu.Choix_Coup,
 				  MyPuissance4.Coup_Joueur2);
    use MyPartie;
    
